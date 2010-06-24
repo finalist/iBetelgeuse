5317ddbbdc56d2ace0fb84d71f5854607071712d
Peter van der Tak <seye@xs4all.nl>
2010-06-24 15:17:08 +0200

    Tested ARDelayFilter.

---
+++ Sources/Tests/ARFilterTest.m
@@ -0,0 +1,47 @@
+//
+//  ARFilterTest.m
+//  iBetelgeuse
+//
+//  Copyright 2010 Finalist IT Group. All rights reserved.
+//
+//  This file is part of iBetelgeuse.
+//  
+//  iBetelgeuse is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  iBetelgeuse is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with iBetelgeuse.  If not, see <http://www.gnu.org/licenses/>.
+//
+
+#import "ARFilterTest.h"
+#import "ARFilter.h"
+#import <GHUnit/GHUnit.h>
+
+
+@implementation ARFilterTest
+
+#pragma mark GHTestCase
+
+- (void)testConstantInputWithFilterFactory:(ARFilterFactory *)filterFactory input:(ARFilterValue)input sampleCount:(int)sampleCount accuracy:(ARFilterValue)accuracy {
+	ARFilter *filter = [filterFactory newFilter];
+	for (int i = 0; i < sampleCount; ++i) {
+		NSTimeInterval timestamp = 100 + floor(i/3); // Give the same value three times in a row, before updating to a new timestamp.
+		ARFilterValue output = [filter filterWithInput:input timestamp:timestamp];
+		GHAssertEqualsWithAccuracy(output, input, accuracy, nil);
+	}
+	[filter release];
+}
+
+- (void)testSimpleCasesWithFilterFactory:(ARFilterFactory *)filterFactory sampleCount:(int)sampleCount accuracy:(ARFilterValue)accuracy {
+	[self testConstantInputWithFilterFactory:filterFactory input:0. sampleCount:sampleCount accuracy:accuracy];
+	[self testConstantInputWithFilterFactory:filterFactory input:-123.45 sampleCount:sampleCount accuracy:accuracy];
+}
+
+@end
